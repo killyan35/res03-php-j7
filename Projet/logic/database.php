ee20e99bf8daf6f5a2908e54d7c953827c212b3d
killyan35 <kilyan.gerard@3wa.io>
2023-02-03 13:39:37 +0000

    probleme database resolu

--- Projet/logic/database.php
+++ Projet/logic/database.php
@@ -21,32 +21,28 @@ $db = new PDO(
     $users = $query->fetch(PDO::FETCH_ASSOC);
     
 }
-function saveUser(User $user) : User
-{
-    $host = "db.3wa.io";
-$port = "3306";
-$dbname = "kilyangerard_phpj7";
-$connexionString = "mysql:host=$host;port=$port;dbname=$dbname";
-
-$user = "kilyangerard";
-$password = "e17f39e5cb4de95dba99a2edd6835ab4";
-
-$db = new PDO(
-    $connexionString,
-    $user,
-    $password
-);
+function saveUser(User $user){
+    $db = new PDO(
+        "mysql:host=db.3wa.io;port=3306;dbname=kilyangerard_phpj7",
+        "kilyangerard",
+        "e17f39e5cb4de95dba99a2edd6835ab4"
+    );
     $query = $db->prepare('INSERT INTO users VALUES (null, :value1, :value2, :value3, :value4)');
     $parameters = [
-        'value1' => $user->getFirst_name(),
-        'value2' => $user->getLast_name(),
-        'value3' => $user->getEmail(),
-        'value4' => $user->getPassword()
-        ];
+    'value1' => $user->getFirst_name(),
+    'value2' => $user->getLast_name(),
+    'value3' => $user->getEmail(),
+    'value4' => $user->getPassword()
+    ];
     $query->execute($parameters);
+    // $userAdded = $query->fetch(PDO::FETCH_ASSOC);
+    // var_dump ($userAdded);
+    
 }
 
-$newuser = new User("killyan","gerard","killyan.gerard@test.fr", 123 );
-var_dump($newuser);
-saveUser($newuser);
+$userToSave = new User("Killyan", "Gerard", "to@test.com", 124);
+var_dump ($userToSave);
+saveUser($userToSave);
+var_dump ($userToSave);
+
 ?>
--- Projet/models/User.php
+++ Projet/models/User.php
@@ -1,16 +1,17 @@
-<?php 
+<?php
 class User {
 
     // private attribute
     private int $id;
     private string $first_name;
     private string $last_name;
-    private string $email; 
+    private string $email;
     private string $password;
+
     // public constructor
     public function __construct(string $first_name, string $last_name, string $email, string $password)
     {
-        $this->id = -1 ;
+        $this->id = -1;
         $this->first_name = $first_name;
         $this->last_name = $last_name;
         $this->email = $email;
@@ -22,60 +23,43 @@ class User {
     {
         return $this->id;
     }
-
-    // public setter
-    public function setId(int $id) : int
-    {
-        $this->id = $id;
-    }
-    
-    
-    // public getter
     public function getFirst_name() : string
     {
-        return $this ->first_name;
+        return $this->first_name;
     }
-    // public setter
-    public function setFirst_name(string $first_name) : string
-    {
-        $this->first_name = $first_name;
-    }
-    
-    
-    // public getter
     public function getLast_name() : string
     {
         return $this->last_name;
     }
-    // public setter
-    public function setLast_name(string $last_name) : string
-    {
-        $this->last_name = $last_name;
-    }
-    
-    
-    // public getter
     public function getEmail() : string
     {
         return $this->email;
     }
-
-    // public setter
-    public function setEmail(string $email) : string
-    {
-        $this->email = $email;
-    }
-    // public getter
     public function getPassword() : string
     {
         return $this->password;
     }
 
     // public setter
-    public function setPassword(string $password) : string
+    public function setId(int $id) : void
+    {
+        $this->id = $id;
+    }
+    public function setFirst_name(string $first_name) : void
+    {
+        $this->first_name = $first_name;
+    }
+    public function setLast_name(string $last_name) : void
+    {
+        $this->last_name = $last_name;
+    }
+    public function setEmail(string $email) : void
+    {
+        $this->email = $email;
+    }
+    public function setPassword(string $password) : void
     {
         $this->password = $password;
     }
-
 }
 ?>
